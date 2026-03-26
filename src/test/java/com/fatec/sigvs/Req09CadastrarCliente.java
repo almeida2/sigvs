47670e8ec172eb5347eac81dea6ffaf50cc0ac25
almeida2 <projetodidaticovl@gmail.com>
2026-03-26 11:47:58 -0300

    feat: cadastrar cliente

---
+++ src/test/java/com/fatec/sigvs/Req09CadastrarCliente.java
@@ -0,0 +1,20 @@
+package com.fatec.sigvs;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.fatec.sigvs.model.ValidaCpf;
+
+public class Req09CadastrarCliente {
+    @Test
+    public void ct01_cadastrar_cliente_com_cpf_valido() {
+        // dado que as informacoes de cliente sao validas
+        ClienteDTO cliente = new ClienteDTO("95388326047", "Joao", "12345678", "Rua 1", "Bairro 1", "Cidade 1",
+                "Complemento 1", "joao@gmail.com");
+        // quando o cliente é cadastrado
+        ValidaCpf cpf = new ValidaCpf(cliente.cpf());
+        // então o cpf é valido
+        assertTrue(cpf.isValido());
+    }
+}
