bc771c9e0591ea7e8e60379358c30eb1d2a549d6
almeida2 <projetodidaticovl@gmail.com>
2026-03-27 11:02:27 -0300

    test: refatoracao dos testes parametrizados

--- src/test/java/com/fatec/sigvs/TIReq09CadastrarCliente3Tests.java
+++ src/test/java/com/fatec/sigvs/TIReq09CadastrarCliente3Tests.java
@@ -8,37 +8,28 @@ import com.fatec.sigvs.model.Cliente;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TIReq09CadastrarCliente3Tests {
+    // entidade
+    private Cliente cliente;
 
-    @ParameterizedTest(name = "Registro {index}: CPF {0} - Nome {1}")
-    @CsvFileSource(files = "C:/edson/dados/clientes.csv", numLinesToSkip = 1)
-    public void ct01_cadastrar_clientes_csv(
-            String cpf,
-            String nome,
-            String cep,
-            String endereco,
-            String bairro,
-            String cidade,
-            String complemento,
-            String email) {
-
-        // 1. Dado que as informações do CSV são lidas
-        ClienteDTO clienteDto = new ClienteDTO(cpf, nome, cep, endereco, bairro, cidade, complemento, email);
-
-        // 2. Quando o cliente é instanciado/processado
-        Cliente c = new Cliente();
-        c.setCpf(clienteDto.cpf());
-        c.setNome(clienteDto.nome());
-        c.setCep(clienteDto.cep());
-        c.setEndereco(clienteDto.endereco());
-        c.setBairro(clienteDto.bairro());
-        c.setCidade(clienteDto.cidade());
-        c.setComplemento(clienteDto.complemento());
-        c.setEmail(clienteDto.email());
-        c.setDataCadastro();
-
-        // 3. Então o assert é apresentado individualmente para cada linha no relatório
-        // do JUnit
-        assertNotNull(c, "O cliente não deveria ser nulo para o CPF: " + cpf);
-        assertNotNull(c.getDataCadastro(), "A data de cadastro deve ser preenchida");
+    @ParameterizedTest(name = "Registro {index}: CPF {0}")
+    @CsvFileSource(resources = "/dataset10.csv", numLinesToSkip = 1)
+    public void ct_verifica_comportamento_cadastro(String cpf, String nome, String cep, String endereco, String bairro,
+            String cidade, String complemento, String email, String re) {
+        try {
+            cliente = new Cliente();
+            cliente.setCpf(cpf);
+            cliente.setNome(nome);
+            cliente.setCep(cep);
+            cliente.setEndereco(endereco);
+            cliente.setComplemento(complemento);
+            cliente.setBairro(bairro);
+            cliente.setCidade(cidade);
+            cliente.setDataCadastro();
+            cliente.setEmail(email);
+            assertNotNull(cliente);
+            assertEquals(re, "satisfatorio");
+        } catch (Exception e) {
+            assertEquals(re, e.getMessage());
+        }
     }
 }
