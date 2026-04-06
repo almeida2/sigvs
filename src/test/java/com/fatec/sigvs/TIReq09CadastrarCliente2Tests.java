d7d1c6dec4d2061bc76fe78d9e20aed7ccc6466d
almeida2 <projetodidaticovl@gmail.com>
2026-04-06 16:44:40 -0300

    test:i nclusao de casos para validar o comportamento na entrada de dados

--- src/test/java/com/fatec/sigvs/TIReq09CadastrarCliente2Tests.java
+++ src/test/java/com/fatec/sigvs/TIReq09CadastrarCliente2Tests.java
@@ -1,23 +1,27 @@
 package com.fatec.sigvs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fatec.sigvs.model.Cliente;
 
 /**
  * Objetivo de teste - validar a integração entre a classe Cliente e a ValidaCPF
- * Segue uma abordagem de desenvolvimento orientado a testes de aceitação.
+ * Segue uma abordagem de desenvolvimento orientado a testes de aceitação
+ * utilizando o padrão Gherkin.
  */
 public class TIReq09CadastrarCliente2Tests {
-    @Test
-    public void ct01_cadastrar_cliente_com_cpf_valido() {
-        // dado que as informacoes de cliente sao validas
-        ClienteDTO cliente = new ClienteDTO("95388326047", "Joao", "12345678", "Rua 1", "Bairro 1", "Cidade 1",
+    ClienteDTO cliente;
+    Cliente c;
+
+    @BeforeEach
+    public void setup() {
+        cliente = new ClienteDTO("95388326047", "Joao", "12345678", "Rua 1", "Bairro 1", "Cidade 1",
                 "Complemento 1", "joao@gmail.com");
-        // quando o cliente é cadastrado
-        Cliente c = new Cliente();
+        c = new Cliente();
         c.setCpf(cliente.cpf());
         c.setNome(cliente.nome());
         c.setCep(cliente.cep());
@@ -27,8 +31,28 @@ public class TIReq09CadastrarCliente2Tests {
         c.setComplemento(cliente.complemento());
         c.setEmail(cliente.email());
         c.setDataCadastro();
+    }
+
+    @Test
+    public void ct01_cadastrar_cliente_com_cpf_valido() {
+        // dado que as informacoes de cliente sao validas
+        // quando a operação é confirmada
+        setup();
         // então o cliente é cadastrado
         assertNotNull(c);
 
+    }
+
+    @Test
+    public void ct02_cadastrar_cliente_com_cpf_em_branco() {
+        setup();
+        try {
+            // dado que as informações do cliente são invalidas
+            // quando a operação é confirmada
+            c.setCpf("");
+        } catch (Exception e) {
+            // então mensagem cliente invalido
+            assertEquals("CPF invalido", e.getMessage());
+        }
     }
 }
